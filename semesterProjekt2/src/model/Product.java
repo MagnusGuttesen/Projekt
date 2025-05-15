156416520b6085e5a55d6abf62df5d8c499396ab
Muuse23 <10641029@ucn.dk>
2025-05-15 19:43:51 +0200

    Revert "ProductDB, ProductController, ProductDBIF"
    This reverts commit 4d09f8f19fd84b38682af0e4a156f8af2924c674.

--- semesterProjekt2/src/model/Product.java
+++ semesterProjekt2/src/model/Product.java
@@ -14,10 +14,6 @@ public class Product {
 		this.skuNo = skuNo;
 		this.productQty = productQty;
 	}
-	//Den her er så vi kan lave en ny instans af product objektet uden at definere værdier
-	public Product() {
-
-	}
 	
 	public int getProductId() {
 		return productId;
